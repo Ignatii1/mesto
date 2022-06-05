289d1c261e0838e25e06467070b8b9bfb02fd47b
Ignatii1 <qinfen@ya.ru>
2022-06-06 00:06:25 +0300

    ready for first review iteration

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 const edit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
+let userName = document.querySelector('.profile__name');
+let userDescription = document.querySelector('.profile__description');
+let inputName = document.querySelector('.popup__input-name');
+let inputDescription = document.querySelector('.popup__input-description');
+let formElement = document.querySelector('.popup__container');
 
-console.log(edit);
+console.log(userName.innerText);
 
 edit.addEventListener('click', function() {
-  popup.classList.remove('popup_hidden');
+  popup.classList.add('popup_opened');
+  inputName.value = userName.innerText;
+  inputDescription.value = userDescription.innerText;
 })
 
 popupCloseButton.addEventListener('click', function() {
-  popup.classList.add('popup_hidden');
+  popup.classList.remove('popup_opened');
+})
+
+formElement.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  userName.innerText = inputName.value;
+  userDescription.innerText = inputDescription.value;
+  console.log(formElement.innerText);
+  popup.classList.remove('popup_opened');
 })
